86fdc9c2d362902df683a3c3122820e1900d2921
adhan-857 <adhan.andika857@gmail.com>
2023-09-13 11:41:42 +0700

    Menambahkan unit test

--- main/tests.py
+++ main/tests.py
@@ -9,3 +9,12 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    # Testing tambahan
+    def test_title_web_ditampilkan(self):
+        response = response = Client().get('/main/')
+        self.assertContains(response,'- Pachill Market Stock Manager -')
+
+    def test_deskripsi_ditampilkan(self):
+        response = response = Client().get('/main/')
+        self.assertContains(response,'Platform pengelola stok supermarket idaman kamu :D')
